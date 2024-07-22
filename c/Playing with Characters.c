8e0290cfdbb50ff57e34e839e55e1a4a885404c5
Manabe0304 <hainamn833@gmail.com>
2024-07-22 18:20:42 +0700

    Create Playing with Characters.c

---
+++ c/Playing with Characters.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch, s[100], sen[1000];
+    scanf("%c", &ch);
+    printf("%c\n", ch);
+    scanf("%s", &s);
+    printf("%s\n", s);
+    getchar();
+    scanf("%[^\n]%*c", &sen);
+    printf("%s", sen);
+    return 0;
+}
+
+/* 
+Sample Input 
+C
+Language
+Welcome To C!!
+
+Sample Output 
+C
+Language
+Welcome To C!!
+*/
